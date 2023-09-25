7cfbba302116e2b8100cb28f9f7543452c002d06
Ravina Dhruve <136399755+ravinadhruve10@users.noreply.github.com>
2023-09-25 12:20:12 -0700

    Fix and cleanup trust-relationship from service-principal module (#5)

--- modules/services/service-principal/organizational.tf
+++ modules/services/service-principal/organizational.tf
@@ -14,7 +14,7 @@ data "google_organization" "org" {
 #---------------------------------
 # role permissions for onboarding
 #---------------------------------
-resource "google_organization_iam_member" "onboarding_role" {
+resource "google_organization_iam_member" "browser" {
   count = var.is_organizational ? 1 : 0
 
   org_id = data.google_organization.org[0].org_id
@@ -22,10 +22,10 @@ resource "google_organization_iam_member" "onboarding_role" {
   member = "serviceAccount:${google_service_account.sa.email}"
 }
 
-#--------------------------------------------------------------------------------------
-# role permissions for CSPM (GCP Predefined Roles for Sysdig Cloud Trust Relationship)
-#--------------------------------------------------------------------------------------
-resource "google_organization_iam_member" "trust_relationship_role" {
+#---------------------------------------------------------------------------------------------
+# role permissions for CSPM (GCP Predefined Roles for Sysdig Cloud Secure Posture Management)
+#---------------------------------------------------------------------------------------------
+resource "google_organization_iam_member" "cloudasset_viewer" {
   for_each = var.is_organizational ? toset(["roles/cloudasset.viewer"]) : []
 
   org_id = data.google_organization.org[0].org_id
@@ -36,7 +36,7 @@ resource "google_organization_iam_member" "trust_relationship_role" {
 #---------------------------------------------------------------------------------------
 # role permissions for CIEM (GCP Predefined Roles for Sysdig Cloud Identity Management)
 #---------------------------------------------------------------------------------------
-resource "google_organization_iam_member" "identity_mgmt_role" {
+resource "google_organization_iam_member" "identity_mgmt" {
   for_each = var.is_organizational ? toset(["roles/recommender.viewer", "roles/iam.serviceAccountViewer", "roles/iam.organizationRoleViewer"]) : []
 
   org_id = data.google_organization.org[0].org_id
