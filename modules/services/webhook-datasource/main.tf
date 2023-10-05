b75ad11b5e59e99d1459e0670bd31880131b837b
Giovanni Recupero <giovanni.recupero@sysdig.com>
2023-10-05 18:11:46 +0200

    Create organizational template file and refactor

--- modules/services/webhook-datasource/main.tf
+++ modules/services/webhook-datasource/main.tf
@@ -49,25 +49,6 @@ resource "google_project_iam_audit_config" "audit_config" {
   }
 }
 
-resource "google_organization_iam_audit_config" "audit_config" {
-  count = var.is_organizational ? 1 : 0
-
-  org_id  = var.organization_id
-  service = "allServices"
-
-  audit_log_config {
-    log_type = "ADMIN_READ"
-  }
-
-  audit_log_config {
-    log_type = "DATA_READ"
-  }
-
-  audit_log_config {
-    log_type = "DATA_WRITE"
-  }
-}
-
 #-----------------#
 # Ingestion topic #
 #-----------------#
@@ -105,28 +86,11 @@ resource "google_logging_project_sink" "ingestion_sink" {
   unique_writer_identity = true
 }
 
-resource "google_logging_organization_sink" "ingestion_sink" {
-  count = var.is_organizational ? 1 : 0
-
-  name        = "${google_pubsub_topic.ingestion_topic.name}_sink"
-  description = "Sysdig sink to direct the AuditLogs to the PubSub topic used for data gathering"
-  org_id      = var.organization_id
-
-  # NOTE: The target destination is a PubSub topic
-  destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${google_pubsub_topic.ingestion_topic.name}"
-  filter      = "protoPayload.@type = \"type.googleapis.com/google.cloud.audit.AuditLog\""
-
-  # NOTE: The include_children attribute is set to true in order to ingest data
-  # even from potential sub-organizations
-  include_children = true
-}
-
 resource "google_pubsub_topic_iam_member" "publisher_iam_member" {
   project = google_pubsub_topic.ingestion_topic.project
   topic   = google_pubsub_topic.ingestion_topic.name
   role    = "roles/pubsub.publisher"
-  # member  = google_logging_project_sink.ingestion_sink.writer_identity
-  member = var.is_organizational ? google_logging_organization_sink.ingestion_sink[0].writer_identity : google_logging_project_sink.ingestion_sink[0].writer_identity
+  member  = var.is_organizational ? google_logging_organization_sink.ingestion_sink[0].writer_identity : google_logging_project_sink.ingestion_sink[0].writer_identity
 }
 
 #-------------------#
---
+++ modules/services/webhook-datasource/organizational.tf
@@ -0,0 +1,42 @@
+#------------#
+# Audit Logs #
+#------------#
+
+resource "google_organization_iam_audit_config" "audit_config" {
+  count = var.is_organizational ? 1 : 0
+
+  org_id  = var.organization_id
+  service = "allServices"
+
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+
+  audit_log_config {
+    log_type = "DATA_READ"
+  }
+
+  audit_log_config {
+    log_type = "DATA_WRITE"
+  }
+}
+
+#------#
+# Sink #
+#------#
+
+resource "google_logging_organization_sink" "ingestion_sink" {
+  count = var.is_organizational ? 1 : 0
+
+  name        = "${google_pubsub_topic.ingestion_topic.name}_sink"
+  description = "Sysdig sink to direct the AuditLogs to the PubSub topic used for data gathering"
+  org_id      = var.organization_id
+
+  # NOTE: The target destination is a PubSub topic
+  destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${google_pubsub_topic.ingestion_topic.name}"
+  filter      = "protoPayload.@type = \"type.googleapis.com/google.cloud.audit.AuditLog\""
+
+  # NOTE: The include_children attribute is set to true in order to ingest data
+  # even from potential sub-organizations
+  include_children = true
+}
