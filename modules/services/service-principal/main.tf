23e9cfcdc26b2fa40515db2011e3c4ad9af4d1b3
Ravina Dhruve <ravina.dhruve@sysdig.com>
2023-10-30 15:27:58 -0700

    (internal) Consolidate roles and permissions assignment to SA

--- modules/services/service-principal/main.tf
+++ modules/services/service-principal/main.tf
@@ -8,33 +8,16 @@ resource "google_service_account" "sa" {
   display_name = "Service account for secure posture management"
 }
 
-#---------------------------------
+#---------------------------------------------------------------------------------------------
 # role permissions for onboarding
-#---------------------------------
-resource "google_project_iam_member" "browser" {
-  count = var.is_organizational ? 0 : 1
-
-  project = var.project_id
-  role    = "roles/browser"
-  member  = "serviceAccount:${google_service_account.sa.email}"
-}
-
-#---------------------------------------------------------------------------------------------
 # role permissions for CSPM (GCP Predefined Roles for Sysdig Cloud Secure Posture Management)
-#---------------------------------------------------------------------------------------------
-resource "google_project_iam_member" "cloudasset_viewer" {
-  for_each = var.is_organizational ? [] : toset(["roles/cloudasset.viewer", "roles/iam.serviceAccountTokenCreator"])
-
-  project = var.project_id
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.sa.email}"
-}
-
-#---------------------------------------------------------------------------------------
 # role permissions for CIEM (GCP Predefined Roles for Sysdig Cloud Identity Management)
-#---------------------------------------------------------------------------------------
-resource "google_project_iam_member" "identity_mgmt" {
-  for_each = var.is_organizational ? [] : toset(["roles/recommender.viewer", "roles/iam.serviceAccountViewer", "roles/iam.roleViewer", "roles/container.clusterViewer", "roles/compute.viewer"])
+#---------------------------------------------------------------------------------------------
+resource "google_project_iam_member" "onboarding_posture_identity-mgmt" {
+  for_each = var.is_organizational ? [] : toset([
+    "roles/browser",
+    "roles/cloudasset.viewer", "roles/iam.serviceAccountTokenCreator",
+    "roles/recommender.viewer", "roles/iam.serviceAccountViewer", "roles/iam.roleViewer", "roles/container.clusterViewer", "roles/compute.viewer"])
 
   project = var.project_id
   role    = each.key
--- modules/services/service-principal/organizational.tf
+++ modules/services/service-principal/organizational.tf
@@ -11,33 +11,16 @@ data "google_organization" "org" {
 # Setup Service Account permissions
 ###################################################
 
-#---------------------------------
+#---------------------------------------------------------------------------------------------
 # role permissions for onboarding
-#---------------------------------
-resource "google_organization_iam_member" "browser" {
-  count = var.is_organizational ? 1 : 0
-
-  org_id = data.google_organization.org[0].org_id
-  role   = "roles/browser"
-  member = "serviceAccount:${google_service_account.sa.email}"
-}
-
-#---------------------------------------------------------------------------------------------
 # role permissions for CSPM (GCP Predefined Roles for Sysdig Cloud Secure Posture Management)
-#---------------------------------------------------------------------------------------------
-resource "google_organization_iam_member" "cloudasset_viewer" {
-  for_each = var.is_organizational ? toset(["roles/cloudasset.viewer", "roles/iam.serviceAccountTokenCreator"]) : []
-
-  org_id = data.google_organization.org[0].org_id
-  role   = each.key
-  member = "serviceAccount:${google_service_account.sa.email}"
-}
-
-#---------------------------------------------------------------------------------------
 # role permissions for CIEM (GCP Predefined Roles for Sysdig Cloud Identity Management)
-#---------------------------------------------------------------------------------------
-resource "google_organization_iam_member" "identity_mgmt" {
-  for_each = var.is_organizational ? toset(["roles/recommender.viewer", "roles/iam.serviceAccountViewer", "roles/iam.organizationRoleViewer", "roles/container.clusterViewer", "roles/compute.viewer"]) : []
+#---------------------------------------------------------------------------------------------
+resource "google_organization_iam_member" "onboarding_posture_identity-mgmt" {
+  for_each = var.is_organizational ? toset([
+    "roles/browser",
+    "roles/cloudasset.viewer", "roles/iam.serviceAccountTokenCreator",
+    "roles/recommender.viewer", "roles/iam.serviceAccountViewer", "roles/iam.organizationRoleViewer", "roles/container.clusterViewer", "roles/compute.viewer"]) : []
 
   org_id = data.google_organization.org[0].org_id
   role   = each.key
