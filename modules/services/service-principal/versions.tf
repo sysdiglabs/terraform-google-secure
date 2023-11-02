5dce873b7949db7e65fe2fac633966de4829734b
Ravina Dhruve <ravina.dhruve@sysdig.com>
2023-11-02 16:46:39 -0700

    Minor fix with required provider block

--- modules/services/service-principal/versions.tf
+++ modules/services/service-principal/versions.tf
@@ -6,8 +6,6 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 4.21.0"
     }
-  }
-  required_providers {
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.1"
