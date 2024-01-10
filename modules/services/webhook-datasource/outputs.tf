43dc2d41288c748684f9175a8cf14268ba1f580b
Ravina Dhruve <ravina.dhruve@sysdig.com>
2024-01-09 18:25:48 -0800

    feat: Add module outputs for webhook-datasource

--- modules/services/webhook-datasource/outputs.tf
+++ modules/services/webhook-datasource/outputs.tf
@@ -2,3 +2,18 @@ output "push_endpoint" {
   value       = google_pubsub_subscription.ingestion_topic_push_subscription.push_config[0].push_endpoint
   description = "Push endpoint towards which the POST request will be directed"
 }
+
+output "ingestion_pubsub_topic_name" {
+  value       = google_pubsub_topic.ingestion_topic.name
+  description = "PubSub ingestion topic that will hold all the AuditLogs coming from the specified project"
+}
+
+output "ingestion_sink_name" {
+  value       = google_logging_project_sink.ingestion_sink.name
+  description = "Project/Organization sink to direct the AuditLogs towards a dedicated PubSub topic"
+}
+
+output "ingestion_push_subscription_name" {
+  value       = google_pubsub_subscription.ingestion_topic_push_subscription.name
+  description = "Push Subscription that will POST the AuditLogs collected from the project towards Sysdig's backend"
+}
