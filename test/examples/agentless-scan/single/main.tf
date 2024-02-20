a5bddca74ea453e75c23e8437822ff5068507363
iru <irune.prado@sysdig.com>
2024-02-20 12:37:53 +0100

    chore: update usage

--- test/examples/agentless-scan/single/main.tf
+++ test/examples/agentless-scan/single/main.tf
@@ -3,6 +3,11 @@ provider "google" {
   region  = "us-west1"
 }
 
+provider "sysdig" {
+  sysdig_secure_url       = "https://secure-staging.sysdig.com"
+  sysdig_secure_api_token = "12124235"
+}
+
 module "agentless-scan" {
   source          = "../../../..//modules/services/agentless-scan"
   project_id      = "mytestproject"
