105a2e20c18f546577797a65d3e3175dad074a5a
iru <irune.prado@sysdig.com>
2024-03-06 16:24:38 +0100

    ci: lint fixes

--- modules/services/agentless-scan/data.tf
+++ modules/services/agentless-scan/data.tf
@@ -6,8 +6,3 @@ data "google_organization" "org" {
   count  = local.is_organizational ? 1 : 0
   domain = var.organization_domain
 }
-
-data "google_projects" "all_projects" {
-  count  = local.is_organizational ? 1 : 0
-  filter = "parent.id:${data.google_organization.org[0].org_id} lifecycleState:ACTIVE"
-}
