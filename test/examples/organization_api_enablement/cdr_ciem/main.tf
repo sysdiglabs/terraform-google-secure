ee502b5eaef0d2bb345562cf8a34b617584b0db1
Jose Pablo Camacho <jose.camacho@sysdig.com>
2024-09-09 15:21:45 -0600

    feat(modular): add TF snippets for enable GCP APIs

--- test/examples/organization_api_enablement/cdr_ciem/main.tf
+++ test/examples/organization_api_enablement/cdr_ciem/main.tf
@@ -13,10 +13,13 @@ locals {
   services = [
     "pubsub.googleapis.com"
   ]
+  root_projects = [for project in data.google_projects.organization_projects.projects : project.project_id]
+  folder_projects = jsondecode(data.local_file.projects_from_folder.content)
+  all_projects = concat(local.root_projects, local.folder_projects)
   project_and_services = flatten([
-    for project in data.google_projects.organization_projects.projects : [
+    for project in local.all_projects : [
       for service in local.services : {
-        project = project.project_id
+        project = project
         service = service
       }
     ]
@@ -31,6 +34,11 @@ provider "google" {
 # Get list of projects under the specified organization
 data "google_projects" "organization_projects" {
   filter = "parent.type:organization parent.id:${local.organizationID}"
+}
+
+data "local_file" "projects_from_folder" {
+  filename = "project_ids.json"
+  depends_on = [null_resource.get_projects_from_folders]
 }
 
 # Enable API services for GCP project
@@ -50,4 +58,72 @@ output "enabled_projects" {
 
 output "enabled_services" {
   value = distinct([for service in google_project_service.enable_cdr_ciem_apis : service.service])
+}
+
+# Script to get projects from folders recursively and set to a file
+resource "null_resource" "get_projects_from_folders" {
+  provisioner "local-exec" {
+    command = <<EOF
+    #!/bin/bash
+    ORG_ID="933620940614"
+
+    # array to store project IDs
+    declare -a FINAL_PROJECT_IDS
+
+    list_projects() {
+      local folder_id=$1
+
+      # get projects from folder
+      local projects_json=$(gcloud projects list --filter="parent.id=$folder_id AND parent.type=folder" --format=json)
+
+      # check valid array
+      if ! echo "$projects_json" | jq empty >/dev/null 2>&1; then
+        echo "Invalid JSON returned for projects list."
+        return
+      fi
+
+      # get project ids
+      local project_ids=$(echo "$projects_json" | jq -r '.[] | .projectId')
+
+      # check project ids not empty and add to global variable
+      if [ -n "$project_ids" ]; then
+        for project_id in $project_ids; do
+          FINAL_PROJECT_IDS+=("$project_id")
+        done
+      else
+        echo "No projects found in folder $folder_id"
+      fi
+    }
+
+    list_folders_recursive() {
+      local parent_id=$1
+      local parent_type=$2
+
+      # list folders on org or other folders
+      if [[ "$parent_type" == "organization" ]]; then
+          folders=$(gcloud resource-manager folders list --organization=$parent_id --format=json)
+      elif [[ "$parent_type" == "folder" ]]; then
+          folders=$(gcloud resource-manager folders list --folder=$parent_id --format=json)
+      fi
+
+      # check if there were folders returned
+      if [ "$(echo "$folders" | jq length)" -eq 0 ]; then
+        return
+      fi
+
+      # iterate over folder and call functions recursively
+      for folder in $(echo "$folders" | jq -c '.[]'); do
+        folder_id=$(echo "$folder" | jq -r '.name' | awk -F'/' '{print $NF}')
+
+        list_projects "$folder_id"
+        list_folders_recursive "$folder_id" "folder"
+      done
+    }
+
+    echo "Listing all projects in folders for organization: $ORG_ID"
+    list_folders_recursive "$ORG_ID" "organization"
+    printf "%s\n" "$${FINAL_PROJECT_IDS[@]}" | jq -R . | jq -s . > "project_ids.json"
+    echo "Projects listed and saved to local file."
+    EOF
+  }
 }
