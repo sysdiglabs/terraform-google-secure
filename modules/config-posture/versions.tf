7d9e1d7b46e63b502afaad252f1c2dc9c1738d56
Haresh Suresh <haresh.suresh@sysdig.com>
2024-09-09 20:47:46 -0700

    bump sysdig provider version to be consistent & have latest datasources

--- modules/config-posture/versions.tf
+++ modules/config-posture/versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     sysdig = {
       source  = "sysdiglabs/sysdig"
-      version = ">= 1.29.2"
+      version = ">= 1.34.0"
     }
     random = {
       source  = "hashicorp/random"
