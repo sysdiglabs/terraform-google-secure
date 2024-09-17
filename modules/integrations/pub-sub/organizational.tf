e35f083eff2ca8198e04ae6c3067f25f30cd0109
jose-pablo-camacho <jose.camacho@sysdig.com>
2024-09-17 10:42:04 -0600

    feat(modular): add modular support for cdr/ciem (#35)
    * feat(modular): add modular support for cdr/ciem
    
    * feat(modular): add modular support for cdr/ciem
    
    * feat(modular): add modular support for cdr/ciem
    
    * feat(modular): add modular support for cdr/ciem
    
    * feat(modular): add modular support for cdr/ciem
    
    * feat(modular): address feedback for  modular support for cdr/ciem
    
    * adding modular onboarding module
    
    * fix var refns
    
    * adding modular onboarding example
    
    * adding config posture module for modular onboarding
    
    * updating README
    
    * fix role naming & version metadata
    
    * updating examples for onboarding & cspm org
    
    * cleanup foundational READMEs
    
    * use external_id datasource
    
    * update README
    
    * remove mgmt_group_ids in cspm module
    
    * bump sysdig provider version to be consistent & have latest datasources
    
    * updating examples
    
    * update example
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * add explicit dependency
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * feat(modular): address feedback for  modular support for cdr/ciem, rebase and cleanup
    
    * enhance(modular): use org domain instead of org ID for gcp api enablement (#40)
    
    * rebase and merge fix
    
    ---------
    
    Co-authored-by: Haresh Suresh <haresh.suresh@sysdig.com>

---
+++ modules/integrations/pub-sub/organizational.tf
@@ -0,0 +1,95 @@
+#-----------------------------------------------------------------------------------------
+# Fetch the data sources
+#-----------------------------------------------------------------------------------------
+
+data "google_organization" "org" {
+  count  = var.is_organizational ? 1 : 0
+  domain = var.organization_domain
+}
+
+#-----------------------------------------------------------------------------------------
+# Audit Logs
+#-----------------------------------------------------------------------------------------
+
+resource "google_organization_iam_audit_config" "audit_config" {
+  for_each = var.is_organizational ? local.audit_log_config : {}
+
+  org_id  = data.google_organization.org[0].org_id
+  service = each.key
+
+  dynamic "audit_log_config" {
+    for_each = each.value.log_config
+    iterator = log_config
+    content {
+      log_type         = log_config.value.log_type
+      exempted_members = log_config.value.exempted_members
+    }
+  }
+}
+
+#-----------------------------------------------------------------------------------------
+# Sink
+#-----------------------------------------------------------------------------------------
+
+resource "google_logging_organization_sink" "ingestion_sink" {
+  count = var.is_organizational ? 1 : 0
+
+  name        = "${google_pubsub_topic.ingestion_topic.name}_sink"
+  description = "Sysdig sink to direct the AuditLogs to the PubSub topic used for data gathering"
+  org_id      = data.google_organization.org[0].org_id
+
+  # NOTE: The target destination is a PubSub topic
+  destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${google_pubsub_topic.ingestion_topic.name}"
+  filter      = var.ingestion_sink_filter
+
+  # Dynamic block to exclude logs from ingestion
+  dynamic "exclusions" {
+    for_each = var.exclude_logs_filter
+    content {
+      name        = exclusions.value.name
+      description = exclusions.value.description
+      filter      = exclusions.value.filter
+      disabled    = exclusions.value.disabled
+    }
+  }
+
+  # NOTE: The include_children attribute is set to true in order to ingest data
+  # even from potential sub-organizations
+  include_children = true
+}
+
+# creating custom role with organization-level permissions to access data ingestion resources
+resource "google_organization_iam_custom_role" "custom_ingestion_auth_role" {
+  count = var.is_organizational ? 1 : 0
+
+  org_id      = data.google_organization.org[0].org_id
+  role_id     = "${local.role_name}Org${local.suffix}"
+  title       = "Sysdigcloud Ingestion Org Auth Role"
+  description = "A Role providing the required permissions for Sysdig Backend to read cloud resources created for data ingestion"
+  permissions = [
+    "pubsub.topics.get",
+    "pubsub.topics.list",
+    "pubsub.subscriptions.get",
+    "pubsub.subscriptions.list",
+    "logging.sinks.get",
+    "logging.sinks.list",
+  ]
+}
+
+# adding custom role with organization-level permissions to the service account for auth
+resource "google_organization_iam_member" "custom" {
+  count = var.is_organizational ? 1 : 0
+
+  org_id = data.google_organization.org[0].org_id
+  role   = google_organization_iam_custom_role.custom_ingestion_auth_role[0].id
+  member = "serviceAccount:${google_service_account.push_auth.email}"
+}
+
+# adding ciem role with permissions to the service account for org
+resource "google_organization_iam_member" "identity_mgmt" {
+  for_each = var.is_organizational ? toset(["roles/recommender.viewer", "roles/iam.serviceAccountViewer", "roles/iam.organizationRoleViewer", "roles/container.clusterViewer", "roles/compute.viewer"]) : []
+
+  org_id = data.google_organization.org[0].org_id
+  role   = each.key
+  member = "serviceAccount:${google_service_account.push_auth.email}"
+}
---
+++ modules/integrations/pub-sub/outputs.tf
@@ -0,0 +1,5 @@
+output "pubsub_datasource_component_id" {
+  value       = "${sysdig_secure_cloud_auth_account_component.gcp_pubsub_datasource.type}/${sysdig_secure_cloud_auth_account_component.gcp_pubsub_datasource.instance}"
+  description = "Component identifier of Webhook Datasource integration created in Sysdig Backend for Log Ingestion"
+  depends_on  = [sysdig_secure_cloud_auth_account_component.gcp_pubsub_datasource]
+}
